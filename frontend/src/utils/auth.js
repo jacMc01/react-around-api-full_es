bcba2bb7b07c9053f82bdc9c961e5dcaba47549d
Jacqueline Mckay H <jacquelinemc@outlook.es>
2023-06-11 17:22:39 -0600

    update api

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = 'http://localhost:4000';
+const baseURL = "https://https://api.mackyuniverse.desarrollointerno.com";
 
 const axiosAuth = axios.create({
   baseURL: baseURL
